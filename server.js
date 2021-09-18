b630a382f0a2c51d369f058f2ec9e13a03df44de
Ahmed Hossam <ahmed_hossam_012@yahoo.com>
2021-09-18 18:27:07 +0200

    Update server.js

--- server.js
+++ server.js
@@ -18,17 +18,17 @@ app.use(express.urlencoded({ extended: false }));
 //routes
 app.use('/api/categories', require('./routes/categories'));
 
-// Wrong path
-app.use((req, res) =>
-  res.status(404).send(`<h1>Can not find what you're looking for</h1>`)
-);
-
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'))
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
 }
+
+// Wrong path
+app.use((req, res) =>
+  res.status(404).send(`<h1>Can not find what you're looking for</h1>`)
+);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port: ${process.env.PORT}`);
